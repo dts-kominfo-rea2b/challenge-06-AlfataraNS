e1686412d0489c1c325b21a50fd7da4b137fcdac
AlfataraNS <alfatara.ns@gmail.com>
2022-06-15 21:41:19 +0700

    solving Challeng! :)

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require('fs');
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -16,9 +17,54 @@ let modifyFile3 = (val) => {
   file3 = val;
 };
 
+const proccessData = (data) => {
+  let msg = "";
+  
+  if (data?.message != undefined) {
+    msg = data?.msg;
+  } else if (data?.length) {
+    data.forEach((item) => {
+      if (item?.msg != undefined) {
+        msg = item?.msg;
+      }
+      if (item?.data?.msg != undefined) {
+        msg = item?.data?.msg;
+      }
+    })
+  }
+  return msg.split(" ")[1];
+}
+
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (fnCallBack) => {
+  let result = [];
+  fs.readFile(file2, {encoding: "utf8"}, (err, data) => {
+    if (err) {
+      fnCallBack(err, null);
+      return;
+    }
+    result.push(proccessData(JSON.parse(data)));
+
+    fs.readFile(file2, {encoding: "utf8"}, (err, data) => {
+      if (err) {
+        fnCallBack(err, null);
+        return;
+      }
+      result.push(proccessData(JSON.parse(data)));
+
+      fs.readFile(file3, {encoding: "utf8"}, (err, data) => {
+        if (err) {
+          fnCallBack(err, null);
+          return;
+        }
+        result.push(proccessData(JSON.parse(data)));
+        
+        fnCallBack(err, result);
+      })
+    })
+  })
+}
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
